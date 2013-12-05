bd71beb5302ff57ab10368ca902c8f7c37f30a6e
Eido NABESHIMA <closer009@gmail.com>
2013-12-05 17:32:34 +0900

    Admin::BaseController

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,7 @@
+class Admin::BaseController < ApplicationController
+  protect_from_forgery with: :exception
+
+  def current_ability
+    @current_ability ||= AdminAbility.new(current_dealer)
+  end
+end
---
+++ test/helpers/admin/base_helper_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class Admin::BaseHelperTest < ActionView::TestCase
+
+  def test_sanity
+    assert true
+  end
+
+end
