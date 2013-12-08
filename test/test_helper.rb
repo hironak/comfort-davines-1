4cbc465cc1c7b15b546bacd2a1f3ca161e84fe74
Eido NABESHIMA <closer009@gmail.com>
2013-12-09 01:28:44 +0900

    SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
+require "simplecov"
+SimpleCov.start 'rails'
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
-
-require "simplecov"
-SimpleCov.start 'rails'
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
