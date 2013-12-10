d5cd155d2b431ccd779f82e2f2cf8557292c9fd7
Eido NABESHIMA <closer009@gmail.com>
2013-12-10 22:22:23 +0900

    よっぱらった2

--- test/controllers/administration/sessions_controller_test.rb
+++ test/controllers/administration/sessions_controller_test.rb
@@ -1,9 +1,24 @@
 require "test_helper"
 
 class Administration::SessionsControllerTest < ActionController::TestCase
+
+  before do
+    @administrator = Administrator.create username: "testuser", password: "password", password_confirmation: "password"
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
+  test "session create" do
+    post :create, username: "testuser", password: "password"
+    assert_redirected_to admin_root_path
+  end
+
+  test "session destroy" do
+    login_user @administrator
+    delete :destroy
+    assert_redirected_to admin_root_path
+  end
 end
