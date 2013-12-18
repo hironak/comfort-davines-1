437dffab34dce2676b85cf9b777fe313342b0fb1
Eido NABESHIMA <closer009@gmail.com>
2013-12-18 16:08:18 +0900

    カート表示 View

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_cart
+
   private
 
   def current_cart
@@ -10,17 +12,17 @@ class ApplicationController < ActionController::Base
   end
 
   def consumer_cart
-    current_consumer || current_consumer.find_or_build_cart
+    current_consumer && current_consumer.find_or_build_cart
   end
 
   def session_cart
     cart =
-      if session["cart_id"]
+      if session["cart.id"]
         Cart.find(session["cart.id"])
       else
         Cart.create
       end
-    session["cart_id"] = cart.id
+    session["cart.id"] = cart.id
     cart
   end
 end
