4d6141a54d70fe5b67d86fd4b931ea2b0f8f64d1
Eido NABESHIMA <closer009@gmail.com>
2013-12-18 17:49:04 +0900

    テスト修正

--- app/controllers/cashier_controller.rb
+++ app/controllers/cashier_controller.rb
@@ -1,23 +1,23 @@
 class CashierController < ApplicationController
   def order
-    redirect_to action: :payment
   end
 
   def order_create
+    redirect_to cashier_payment_url
   end
 
   def payment
-    redirect_to action: :confirm
   end
 
   def payment_create
+    redirect_to cashier_confirm_url
   end
 
   def confirm
-    redirect_to action: :complete
   end
 
   def confirm_create
+    redirect_to cashier_complete_url
   end
 
   def complete
--- test/controllers/cashier_controller_test.rb
+++ test/controllers/cashier_controller_test.rb
@@ -6,14 +6,29 @@ class CashierControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should post order" do
+    post :order_create
+    assert_redirected_to cashier_payment_url
+  end
+
   test "should get payment" do
     get :payment
     assert_response :success
   end
 
+  test "should post payment" do
+    post :payment_create
+    assert_redirected_to cashier_confirm_url
+  end
+
   test "should get confirm" do
     get :confirm
     assert_response :success
+  end
+
+  test "should post confirm" do
+    post :confirm_create
+    assert_redirected_to cashier_complete_url
   end
 
   test "should get complete" do
