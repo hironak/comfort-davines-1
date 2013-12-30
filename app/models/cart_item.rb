9a567e8ba3d6b05bf040a71c3845619217d8ecf3
Eido NABESHIMA <closer009@gmail.com>
2013-12-31 07:08:57 +0900

    CartItem

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,10 @@ class CartItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :product
 
+  def price
+    self.product.price * self.amount
+  end
+
   def increment
     self.amount = 0 unless self.amount
     self.amount += 1
