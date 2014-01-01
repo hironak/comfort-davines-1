b1df7648e1846f31cd0f782cb2e271249957054e
Eido NABESHIMA <closer009@gmail.com>
2014-01-02 07:05:45 +0900

    rubocop

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,7 +6,7 @@ class Cart < ActiveRecord::Base
   has_many :items, class_name: 'CartItem', dependent: :delete_all
   has_many :products, through: :items
 
-  def item_by_product product
+  def item_by_product(product)
     self.items.where(product_id: product.id).first_or_create
   end
 end
