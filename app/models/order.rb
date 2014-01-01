a5444f9cc9e199e2d3c928ed42321e96a407fef4
Eido NABESHIMA <closer009@gmail.com>
2014-01-01 23:41:25 +0900

    トップページとヘッダ

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,9 @@ class Order < ActiveRecord::Base
   include Pricing
   include Regulating
 
-  has_many :items, class_name: 'OrderItem', dependent: :delete_all
   belongs_to :consumer
+  has_many :items, class_name: 'OrderItem', dependent: :delete_all
+  has_many :products, through: :items
 
   accepts_nested_attributes_for :items, allow_destroy: true
 
