57541fc3e9529efdaf5f2161512bc3007bbe1a8d
Eido NABESHIMA <closer009@gmail.com>
2014-01-01 23:42:08 +0900

    boxen

--- config/unicorn/development.rb
+++ config/unicorn/development.rb
@@ -1,7 +1,7 @@
 # config/unicorn.rb
 
 worker_processes 1
-listen "#{ENV['BOXEN_SOCKET_DIR']}/comfort-davines", :backlog => 1024
+listen "#{ENV['BOXEN_SOCKET_DIR'] || '/tmp'}/comfort-davines", :backlog => 1024
 timeout 120
 
 after_fork do |server, worker|
