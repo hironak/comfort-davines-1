754abcf6007f390b98f668dafff49050a025ceda
Eido NABESHIMA <closer009@gmail.com>
2014-01-03 01:54:11 +0900

    delete administration_pages

--- app/models/administration/page.rb
+++
@@ -1,2 +0,0 @@
-class Administration::Page < ActiveRecord::Base
-end
--- test/factories/administration_pages.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :administration_page, :class => 'Administration::Page' do
-    body "MyText"
-    style "MyText"
-  end
-end
