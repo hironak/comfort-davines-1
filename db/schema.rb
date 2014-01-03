4a50664ab6bce7d9c966e641cb2059e44af7eaf3
watarum <watarum@hackers.jp>
2014-01-04 01:12:46 +0900

    Revert "プルダウンにしてみた。"
    This reverts commit a6c3542be4080a5d9f8a0c243f8150db5860ac6c.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20140103065859) do
 
+  create_table "administration_pages", force: true do |t|
+    t.text     "body"
+    t.text     "style"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "administrators", force: true do |t|
     t.string   "username",         null: false
     t.string   "crypted_password"
