f6fe98911b3c98accc20a4bca6ca1f46faac3274
Eido NABESHIMA <closer009@gmail.com>
2014-01-13 09:35:30 +0900

    サロン情報の更新

--- app/controllers/administration/salons_controller.rb
+++ app/controllers/administration/salons_controller.rb
@@ -70,7 +70,7 @@ module Administration
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def salon_params
-      params.require(:salon).permit(:name)
+      params.require(:salon).permit(:number, :name, :grade, :address, :tel, :website)
     end
   end
 end
