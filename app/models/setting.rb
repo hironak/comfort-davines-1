c388737207be21a1247a383ab36a59e49798b072
Eido NABESHIMA <closer009@gmail.com>
2014-01-14 06:35:00 +0900

    消費税の計算

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,4 +1,12 @@
 class Setting < ActiveRecord::Base
   include Authority::Abilities
   self.authorizer_name = 'AdministrationAuthorizer'
+
+  def self.instance
+    self.last
+  end
+
+  def self.method_missing method_name
+    instance.send method_name
+  end
 end
