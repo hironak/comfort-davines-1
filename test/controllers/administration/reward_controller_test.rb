16699ac46b2c3270a13a043b4082cb40caf3e39a
Eido NABESHIMA <closer009@gmail.com>
2014-01-14 07:36:39 +0900

    TEST ALL GREEN

--- test/controllers/administration/reward_controller_test.rb
+++ test/controllers/administration/reward_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class Administration::RewardControllerTest < ActionController::TestCase
+  before do
+    salon = create(:salon)
+    login_user create(:administrator, contractable: salon)
+  end
+
   test "should get show" do
     get :show
     assert_response :success
