43a4310ab8b4362db7c6e0083a8f976f1518462d
Eido NABESHIMA <closer009@gmail.com>
2014-01-22 16:19:47 +0900

    git revision

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'comfort-davines'
-# set :repo_url, 'git@github.com:hackers-jp/comfort-davines.git'
+set :repo_url, 'git@github.com:hackers-jp/comfort-davines.git'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+ask :branch, proc { `git rev-parse HEAD`.chomp }
 
 set :deploy_to, '/var/www/comfort-davines'
 set :scm, :scm
