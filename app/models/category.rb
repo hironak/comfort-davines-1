5d38318927ca6f590ac67bf62f19eab8bf69c4cb
Eido NABESHIMA <closer009@gmail.com>
2014-02-10 00:15:56 +0900

    category itemとか消した

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,4 @@
 class Category < ActiveRecord::Base
   include Authority::Abilities
   self.authorizer_name = 'AdministrationAuthorizer'
-
-  has_many :items
-  accepts_nested_attributes_for :items
 end
--- app/models/series.rb
+++ app/models/series.rb
@@ -3,9 +3,4 @@ class Series < ActiveRecord::Base
   self.authorizer_name = 'AdministrationAuthorizer'
 
   self.table_name = "series"
-
-  has_many :items
-  accepts_nested_attributes_for :items
-
-  has_attached_file :topimage
 end
