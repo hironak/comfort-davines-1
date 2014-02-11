c9b738c3edc2b5a1bfa7c28a38dabbed424b6052
Eido NABESHIMA <closer009@gmail.com>
2014-02-11 11:55:48 +0900

    アイテム表示

--- app/models/product/photo.rb
+++ app/models/product/photo.rb
@@ -1,5 +1,5 @@
 class Product::Photo < ActiveRecord::Base
   belongs_to :product
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "300x300>", item: "230x230<", thumb: "100x100>" }
 end
