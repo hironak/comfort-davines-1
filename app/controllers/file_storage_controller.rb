9fdc83fc4f744339848e99b22d0dbd0e6c1c3ea4
Eido NABESHIMA <closer009@gmail.com>
2014-02-13 22:21:20 +0900

    file storage

---
+++ app/controllers/file_storage_controller.rb
@@ -0,0 +1,21 @@
+class FileStorageController < ApplicationController
+  def show
+    if file_storage = FileStorage.where(file_file_name: file_name).first
+      send_data File.read(file_storage.file.path)
+    elsif Rails.env.development?
+      filename = File.join("#{Rails.root}/presets/**", file_name)
+      if filename = Dir.glob(filename).first
+        send_data File.read(filename)
+      else
+        render text: "not found #{filename}"
+      end
+      return
+    end
+  end
+
+  private
+
+  def file_name
+    "#{params[:filename]}.#{params[:format]}"
+  end
+end
