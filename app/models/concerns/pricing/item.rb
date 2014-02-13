da719e612f453f28a0ec21aa23b656f4e1e06e23
Eido NABESHIMA <closer009@gmail.com>
2014-02-13 22:21:07 +0900

    カート

--- app/models/concerns/pricing/item.rb
+++ app/models/concerns/pricing/item.rb
@@ -1,6 +1,10 @@
 module Pricing::Item
   extend ActiveSupport::Concern
 
+  def view_origin_price
+    (self.origin_price * Setting.tax_rate).to_i
+  end
+
   def price
     (self.origin_price * Setting.tax_rate).to_i * self.amount
   end
