8d1d037d404e67d1551bc207cfcd2bd3f626457a
Eido NABESHIMA <closer009@gmail.com>
2014-02-14 01:48:49 +0900

    deploy/production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,9 +5,9 @@ set :stage, :production
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+# role :app, %w{deploy@example.com}
+# role :web, %w{deploy@example.com}
+# role :db,  %w{deploy@example.com}
 
 # Extended Server Syntax
 # ======================
@@ -15,7 +15,11 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'davines.co.jp', user: 'deploy', roles: %w{web app}
+server '133.242.183.173', user: 'deploy', roles: %w{web app db}, ssh_options: {
+  keys: [ENV['CAP_PRIVATE_KEY'] || "~/.ssh/id_rsa"],
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
