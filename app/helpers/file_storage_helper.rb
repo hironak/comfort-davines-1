b042a1a4f48a3a58e6f703c684df3b3c19f35d19
Eido NABESHIMA <closer009@gmail.com>
2014-02-24 06:14:55 +0900

    storage_file

--- app/helpers/file_storage_helper.rb
+++ app/helpers/file_storage_helper.rb
@@ -1,2 +1,9 @@
 module FileStorageHelper
+  def storage_image_tag src, *options
+    if file_storage = FileStorage.where(file_file_name: src).first
+      image_tag file_storage.file.url, *options
+    else
+      image_tag ''
+    end
+  end
 end
