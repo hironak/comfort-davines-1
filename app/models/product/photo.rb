2b2034870b9abc77a895818fa7f5dfc44fd616b1
Eido NABESHIMA <closer009@gmail.com>
2014-02-28 06:30:41 +0900

    画像サイズ

--- app/models/product/photo.rb
+++ app/models/product/photo.rb
@@ -4,7 +4,7 @@ class Product::Photo < ActiveRecord::Base
   has_attached_file :image,
     styles: {
       medium: "250x380>",
-      item: "230x230<",
+      item: "230x230>",
       thumb: "100x100>",
       mini: "65x65>" }
 
