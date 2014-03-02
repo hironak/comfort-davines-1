cdf305cecf156dab78b1331ef2da5766b3202274
Eido NABESHIMA <closer009@gmail.com>
2014-03-03 02:12:48 +0900

    files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140302130339) do
+ActiveRecord::Schema.define(version: 20140302171216) do
 
   create_table "administrators", force: true do |t|
     t.string   "username",          null: false
@@ -203,6 +203,7 @@ ActiveRecord::Schema.define(version: 20140302130339) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "accepted"
   end
 
   add_index "product_reviews", ["product_id"], name: "index_product_reviews_on_product_id", using: :btree
