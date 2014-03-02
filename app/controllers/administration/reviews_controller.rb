8cd699e9cbd6bc16c28c8b5439abca9e2e7604f5
Eido NABESHIMA <closer009@gmail.com>
2014-03-03 02:39:38 +0900

    test

--- app/controllers/administration/reviews_controller.rb
+++ app/controllers/administration/reviews_controller.rb
@@ -1,54 +1,55 @@
 module Administration
   class ReviewsController < BaseController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+    before_action :set_review, only: [:show, :edit, :update, :destroy]
 
-  # GET /administration/reviews
-  # GET /administration/reviews.json
-  def index
-    @reviews = Product::Review.unscope
-  end
+    # GET /administration/reviews
+    # GET /administration/reviews.json
+    def index
+      @reviews = Product::Review.unscoped
+    end
 
-  # GET /administration/reviews/1
-  # GET /administration/reviews/1.json
-  def show
-  end
+    # GET /administration/reviews/1
+    # GET /administration/reviews/1.json
+    def show
+    end
 
-  # GET /administration/reviews/1/edit
-  def edit
-  end
+    # GET /administration/reviews/1/edit
+    def edit
+    end
 
-  # PATCH/PUT /administration/reviews/1
-  # PATCH/PUT /administration/reviews/1.json
-  def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+    # PATCH/PUT /administration/reviews/1
+    # PATCH/PUT /administration/reviews/1.json
+    def update
+      respond_to do |format|
+        if @review.update(review_params)
+          format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: 'edit' }
+          format.json { render json: @review.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # DELETE /administration/reviews/1
-  # DELETE /administration/reviews/1.json
-  def destroy
-    @review.destroy
-    respond_to do |format|
-      format.html { redirect_to reviews_url }
-      format.json { head :no_content }
+    # DELETE /administration/reviews/1
+    # DELETE /administration/reviews/1.json
+    def destroy
+      @review.destroy
+      respond_to do |format|
+        format.html { redirect_to reviews_url }
+        format.json { head :no_content }
+      end
     end
-  end
 
-  private
+    private
     # Use callbacks to share common setup or constraints between actions.
     def set_review
-      @review = Product::Review.unscope.find(params[:id])
+      @review = Product::Review.unscoped.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
       params.require(:review).permit(:body, :accepted)
     end
+  end
 end
--- test/controllers/administration/reviews_controller_test.rb
+++ test/controllers/administration/reviews_controller_test.rb
@@ -3,48 +3,36 @@ require "test_helper"
 class Administration::ReviewsControllerTest < ActionController::TestCase
 
   before do
-    @administration_review = administration_reviews(:one)
+    login_user create(:administrator)
+    @review = create(:product_review)
   end
 
   def test_index
     get :index
     assert_response :success
-    assert_not_nil assigns(:administration_reviews)
-  end
-
-  def test_new
-    get :new
-    assert_response :success
-  end
-
-  def test_create
-    assert_difference('Administration::Review.count') do
-      post :create, administration_review: {  }
-    end
-
-    assert_redirected_to administration_review_path(assigns(:administration_review))
+    assert_not_nil assigns(:reviews)
   end
 
   def test_show
-    get :show, id: @administration_review
+    get :show, id: @review
     assert_response :success
   end
 
   def test_edit
-    get :edit, id: @administration_review
+    get :edit, id: @review
     assert_response :success
   end
 
   def test_update
-    put :update, id: @administration_review, administration_review: {  }
-    assert_redirected_to administration_review_path(assigns(:administration_review))
+    put :update, id: @review, review: {  }
+    assert_redirected_to admin_review_path(assigns(:review))
   end
 
   def test_destroy
-    assert_difference('Administration::Review.count', -1) do
-      delete :destroy, id: @administration_review
+    assert_difference('Product::Review.count', -1) do
+      delete :destroy, id: @review
     end
 
-    assert_redirected_to administration_reviews_path
+    assert_redirected_to reviews_path
   end
 end
