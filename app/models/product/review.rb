58ea1273834b584c3e235d0f5377df7b5eb5cb86
Eido NABESHIMA <closer009@gmail.com>
2014-03-03 03:26:55 +0900

    管理画面

--- app/models/product/review.rb
+++ app/models/product/review.rb
@@ -1,4 +1,7 @@
 class Product::Review < ActiveRecord::Base
+  include Authority::Abilities
+  self.authorizer_name = 'AdministrationAuthorizer'
+
   belongs_to :product
 
   default_scope -> { where(accepted: true) }
