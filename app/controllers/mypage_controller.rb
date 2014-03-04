8fe0caeea38707b362697262b7fe784e2210c0b2
watarum <watarum@hackers.jp>
2014-03-05 06:47:30 +0900

    もっかいイチからマイページ

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,20 +2,7 @@ class MypageController < ApplicationController
   before_filter :authenticate_consumer!
 
   def index
-    @name = current_consumer.information.name
-  end
-
-  def edit
-    @edit = current_consumer.information.edit
-  end
-
-  def orders
-    @orders = current_consumer.information.orders
-    update
-  end
-
-  def destory
-    @destroy = current_consumer.information.destroy
+    @edit = current_consumer.information
   end
 
 end
