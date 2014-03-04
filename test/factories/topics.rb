e2ea94bc5a7684810d545752a1e99a4beb07e9cc
Eido NABESHIMA <closer009@gmail.com>
2014-03-04 13:53:34 +0900

    topics

---
+++ test/factories/topics.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :topic do
+    date "2014-03-04"
+    subject "MyString"
+    url "MyString"
+  end
+end
