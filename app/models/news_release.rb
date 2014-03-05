93217205c89c307c92c587c014a9ddb9c805b5a8
Eido NABESHIMA <closer009@gmail.com>
2014-03-05 18:05:49 +0900

    news画像を表示

--- app/models/news_release.rb
+++ app/models/news_release.rb
@@ -2,5 +2,7 @@ class NewsRelease < ActiveRecord::Base
   include Authority::Abilities
   self.authorizer_name = 'AdministrationAuthorizer'
 
+  has_attached_file :image, :styles => { :medium => "190x242>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
   scope :available, -> { where("date < ?", Date.tomorrow).order(date: :desc) }
 end
