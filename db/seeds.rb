1b3165c39f46a577062ae300f04f6275ea262612
Eido NABESHIMA <closer009@gmail.com>
2014-03-05 17:05:14 +0900

    雑誌掲載csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,189 +6,202 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Setting.create! tax_percentage: 5
-
-[
-  ['ナチュラルテック', :naturaltech],
-  ['オーセンティック', :authentic],
-  ['オイ オイル',      :oioil],
-  ['モアインサイド',   :moreinside]
-].each do |name, identify|
-  Series.create name: name, identify: identify
-end
-
-[
-  ['キット', :kit],
-  ['シャンプー', ''],
-  ['コンディショナー＆パック', ''],
-  ['スペシャルケア', ''],
-  ['オイル', ''],
-  ['スタイリング', '']
-].each do |name, identify|
-  Category.create name: name, identify: identify
-end
-
-%w|エイジングケア ダメージケア デリケートヘア オイリー・頭皮ケア 保湿ケア|.each do |name|
-  Solution.create name: name
-end
-
-Dir.glob("#{Rails.root}/presets/assets/file_storage/images/**/*").each do |file|
-  if File.file? file
-    name = file.gsub("#{Rails.root}/presets/assets/file_storage/images/", '')
-    FileStorage.find_or_initialize_by(name: name).tap do |storage|
-      storage.file = File.new(file)
-      puts "#{storage.save} #{file}"
-    end
-  end
-end
-
 require "csv"
 
-%w[
-naturaltech_e
-naturaltech_n
-naturaltech_r
-naturaltech_d
-naturaltech_c
-naturaltech_w
-authentic_01
-authentic_02
-authentic_03
-authentic_04
-oioil
-moreinside_01
-moreinside_02
-moreinside_03
-moreinside_04
-moreinside_05
-moreinside_06
-moreinside_07
-].each do |identify|
-  template = ::Template.find_or_initialize_by(identify: identify)
-  template.name = identify
-  header_file = template.send :header_file
-  template.header =
-    if File.exist?(header_file)
-      File.read header_file
-    else
-      ""
-    end
-  style_file = template.send :style_file
-  template.style =
-    if File.exist?(style_file)
-      File.read style_file
-    else
-      ""
-    end
-  template.save validate: false
-end
+# Setting.create! tax_percentage: 5
+#
+# [
+#   ['ナチュラルテック', :naturaltech],
+#   ['オーセンティック', :authentic],
+#   ['オイ オイル',      :oioil],
+#   ['モアインサイド',   :moreinside]
+# ].each do |name, identify|
+#   Series.create name: name, identify: identify
+# end
+#
+# [
+#   ['キット', :kit],
+#   ['シャンプー', ''],
+#   ['コンディショナー＆パック', ''],
+#   ['スペシャルケア', ''],
+#   ['オイル', ''],
+#   ['スタイリング', '']
+# ].each do |name, identify|
+#   Category.create name: name, identify: identify
+# end
+#
+# %w|エイジングケア ダメージケア デリケートヘア オイリー・頭皮ケア 保湿ケア|.each do |name|
+#   Solution.create name: name
+# end
+#
+# Dir.glob("#{Rails.root}/presets/assets/file_storage/images/**/*").each do |file|
+#   if File.file? file
+#     name = file.gsub("#{Rails.root}/presets/assets/file_storage/images/", '')
+#     FileStorage.find_or_initialize_by(name: name).tap do |storage|
+#       storage.file = File.new(file)
+#       puts "#{storage.save} #{file}"
+#     end
+#   end
+# end
+#
+# %w[
+# naturaltech_e
+# naturaltech_n
+# naturaltech_r
+# naturaltech_d
+# naturaltech_c
+# naturaltech_w
+# authentic_01
+# authentic_02
+# authentic_03
+# authentic_04
+# oioil
+# moreinside_01
+# moreinside_02
+# moreinside_03
+# moreinside_04
+# moreinside_05
+# moreinside_06
+# moreinside_07
+# ].each do |identify|
+#   template = ::Template.find_or_initialize_by(identify: identify)
+#   template.name = identify
+#   header_file = template.send :header_file
+#   template.header =
+#     if File.exist?(header_file)
+#       File.read header_file
+#     else
+#       ""
+#     end
+#   style_file = template.send :style_file
+#   template.style =
+#     if File.exist?(style_file)
+#       File.read style_file
+#     else
+#       ""
+#     end
+#   template.save validate: false
+# end
+#
+# CSV.read(Rails.root.join("presets/data/products.csv").to_s, headers: :first_row, col_sep: "\t").each do |attrs|
+#
+#   attrs = attrs.to_hash
+#
+#   files = attrs['image'].to_s.split(/\s/).map do |image|
+#     %w|jpg png|.map { |ext| Rails.root.join("presets/assets/products/image/#{image}.#{ext}") }
+#   end.flatten
+#
+#   files = files.find_all { |f| File.exist?(f) }
+#
+#   attrs.delete 'image'
+#   attrs["photos_attributes"] = []
+#   files.each do |file|
+#     attrs["photos_attributes"] << {
+#       "image" => File.new(file)
+#     }
+#   end
+#
+#   if template_identify = attrs.delete('template')
+#     if template = ::Template.where(identify: template_identify).first
+#       attrs["template_id"] = template.id
+#     end
+#   end
+#
+#   attrs['category_ids'] = attrs['category_ids'].to_s.split
+#
+#   if series = Series.where(identify: attrs['series']).first
+#     attrs['series_ids'] = [series.id]
+#   end
+#   attrs.delete('series')
+#
+#   attrs["stock"] = 10
+#
+#   if identify = attrs.delete('page')
+#     page = Page.find_or_initialize_by(identify: identify)
+#     page.title = attrs["name"]
+#     page.render_type =  "html"
+#     body_file = page.send :body_file
+#     page.body =
+#       if File.exist?(body_file)
+#         File.read body_file
+#       else
+#         ""
+#       end
+#     style_file = page.send :style_file
+#     page.style =
+#       if File.exist?(style_file)
+#         File.read style_file
+#       else
+#         ""
+#       end
+#     page.save validate: false
+#     attrs['page_id'] = page.id
+#   end
+#
+#   singleton = attrs.delete('singleton')
+#
+#   product = Product.create(attrs)
+#
+#   if singleton == '1'
+#     product.series.singleton = product
+#     product.series.save
+#   end
+# end
+#
+# CSV.read(Rails.root.join("presets/data/salons.csv").to_s, headers: :first_row, col_sep: "\t").each do |attrs|
+#
+#   attrs = attrs.to_hash
+#
+#   grades = %w|member platina premium|
+#   attrs["grade"] = grades.detect{|l| attrs["grade_#{l}"] == "○" }
+#   grades.map{|l| "grade_#{l}" }.each{|grade| attrs.delete(grade) }
+#
+#   Salon.create(attrs)
+# end
+#
+# # Development Administrator
+# if (Rails.env.development? || Rails.env.staging?) && Administrator.count == 0
+#   FactoryGirl.find_definitions if require 'factory_girl'
+#   extend FactoryGirl::Syntax::Methods
+#
+#   create :administrator, username: "testuser"
+#
+#   agency = create :agency
+#   create :administrator,
+#     username: "testagency",
+#     contractable: agency
+#
+#   salon = create :salon, agencies: [agency]
+#   create :administrator,
+#     username: "testsalon",
+#     contractable: salon
+#
+#   Product.available.load.each do |product|
+#     product.reviews.create body: "手の届く贅沢です！これなら主婦の私でも続けられます(笑)何より好きなのはこの香り。ずっと洗っていたくなるのでこのシリーズにしてから念入りに頭皮マッサージしちゃいます。顔と一枚皮の頭皮ですから出来るだけオーガニックなものを使いたいです。"
+#   end
+# end
+#
+# NewsRelease.create(
+#   date: "2013-03-20",
+#   title: "3月27日より伊勢丹新宿ビューティアポセカリーにてキット販売中！",
+#   body: "<新商品オイオイルにナチュラルテック>＜N＞のシャンプー、コンディショナーがついた伊勢丹限定キットです。"
+# )
+#
+# NewsRelease.create(
+#   date: "2013-03-01",
+#   title: "3月 6日よりJR 大阪三越伊勢丹で発売開始！",
+#   body: "大阪伊勢丹8階にオーセンティックとナチュラルテック（一部の商品）取扱中。"
+# )
 
-CSV.read(Rails.root.join("presets/data/products.csv").to_s, headers: :first_row, col_sep: "\t").each do |attrs|
+CSV.read(Rails.root.join("presets/data/press_releases.csv").to_s, headers: :first_row, col_sep: "\t").each do |attrs|
 
   attrs = attrs.to_hash
 
-  files = attrs['image'].to_s.split(/\s/).map do |image|
-    %w|jpg png|.map { |ext| Rails.root.join("presets/assets/products/image/#{image}.#{ext}") }
-  end.flatten
+  file_name = attrs.delete('image')
+  month = attrs.delete('month')
+  file_path = "presets/assets/press_release/image/#{month}/#{file_name}"
+  attrs['image'] = File.new(file_path) if File.exist?(file_path)
 
-  files = files.find_all { |f| File.exist?(f) }
-
-  attrs.delete 'image'
-  attrs["photos_attributes"] = []
-  files.each do |file|
-    attrs["photos_attributes"] << {
-      "image" => File.new(file)
-    }
-  end
-
-  if template_identify = attrs.delete('template')
-    if template = ::Template.where(identify: template_identify).first
-      attrs["template_id"] = template.id
-    end
-  end
-
-  attrs['category_ids'] = attrs['category_ids'].to_s.split
-
-  if series = Series.where(identify: attrs['series']).first
-    attrs['series_ids'] = [series.id]
-  end
-  attrs.delete('series')
-
-  attrs["stock"] = 10
-
-  if identify = attrs.delete('page')
-    page = Page.find_or_initialize_by(identify: identify)
-    page.title = attrs["name"]
-    page.render_type =  "html"
-    body_file = page.send :body_file
-    page.body =
-      if File.exist?(body_file)
-        File.read body_file
-      else
-        ""
-      end
-    style_file = page.send :style_file
-    page.style =
-      if File.exist?(style_file)
-        File.read style_file
-      else
-        ""
-      end
-    page.save validate: false
-    attrs['page_id'] = page.id
-  end
-
-  singleton = attrs.delete('singleton')
-
-  product = Product.create(attrs)
-
-  if singleton == '1'
-    product.series.singleton = product
-    product.series.save
-  end
+  attrs['products'] = attrs['products'].gsub(/NN/, "\n")
+  PressRelease.create(attrs)
 end
-
-CSV.read(Rails.root.join("presets/data/salons.csv").to_s, headers: :first_row, col_sep: "\t").each do |attrs|
-
-  attrs = attrs.to_hash
-
-  grades = %w|member platina premium|
-  attrs["grade"] = grades.detect{|l| attrs["grade_#{l}"] == "○" }
-  grades.map{|l| "grade_#{l}" }.each{|grade| attrs.delete(grade) }
-
-  Salon.create(attrs)
-end
-
-# Development Administrator
-if (Rails.env.development? || Rails.env.staging?) && Administrator.count == 0
-  FactoryGirl.find_definitions if require 'factory_girl'
-  extend FactoryGirl::Syntax::Methods
-
-  create :administrator, username: "testuser"
-
-  agency = create :agency
-  create :administrator,
-    username: "testagency",
-    contractable: agency
-
-  salon = create :salon, agencies: [agency]
-  create :administrator,
-    username: "testsalon",
-    contractable: salon
-
-  Product.available.load.each do |product|
-    product.reviews.create body: "手の届く贅沢です！これなら主婦の私でも続けられます(笑)何より好きなのはこの香り。ずっと洗っていたくなるのでこのシリーズにしてから念入りに頭皮マッサージしちゃいます。顔と一枚皮の頭皮ですから出来るだけオーガニックなものを使いたいです。"
-  end
-end
-
-NewsRelease.create(
-  date: "2013-03-20",
-  title: "3月27日より伊勢丹新宿ビューティアポセカリーにてキット販売中！",
-  body: "<新商品オイオイルにナチュラルテック>＜N＞のシャンプー、コンディショナーがついた伊勢丹限定キットです。"
-)
-
-NewsRelease.create(
-  date: "2013-03-01",
-  title: "3月 6日よりJR 大阪三越伊勢丹で発売開始！",
-  body: "大阪伊勢丹8階にオーセンティックとナチュラルテック（一部の商品）取扱中。"
-)
