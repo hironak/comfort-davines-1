628167e31d3527e98558f9aa3bcc7f6991366dfa
Eido NABESHIMA <closer009@gmail.com>
2014-03-05 13:31:23 +0900

    Green

--- test/controllers/contact_controller_test.rb
+++ test/controllers/contact_controller_test.rb
@@ -7,8 +7,7 @@ class ContactControllerTest < ActionController::TestCase
   end
 
   test "should get create" do
-    get :create
+    post :create, contact: { type: '商品について', name: 'name', name_kana: 'kana', email: 'test@example.com', phone: '000-0000-0000', body: 'body' }
     assert_response :success
   end
-
 end
