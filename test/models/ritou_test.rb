9fdfc6f5e1230e4b88e78cb4293138373aeeac0a
Eido NABESHIMA <closer009@gmail.com>
2014-03-06 18:37:26 +0900

    Ritou test

--- test/models/ritou_test.rb
+++ test/models/ritou_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class RitouTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  before do
+    create :ritou, postalcode: "0431400"
+  end
+
+  test "include? true" do
+    assert_equal Ritou.include?("043-1400"), true
+  end
+
+  test "include? false" do
+    assert_equal Ritou.include?("242-0001"), false
+  end
 end
