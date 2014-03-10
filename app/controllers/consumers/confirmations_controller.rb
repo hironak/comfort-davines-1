482f3290e54cb825ce8995bc00019de06b356e92
Eido NABESHIMA <closer009@gmail.com>
2014-03-10 19:07:32 +0900

    noticeの色

--- app/controllers/consumers/confirmations_controller.rb
+++ app/controllers/consumers/confirmations_controller.rb
@@ -1,4 +1,6 @@
 class Consumers::ConfirmationsController < Devise::ConfirmationsController
+  before_filter :set_breadcrumb
+
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
     yield resource if block_given?
@@ -8,5 +10,11 @@ class Consumers::ConfirmationsController < Devise::ConfirmationsController
     else
       respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
     end
+  end
+
+  private
+
+  def set_breadcrumb
+    add_breadcrumb "マイページログイン"
   end
 end
