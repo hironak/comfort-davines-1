ff525ad475b2fcf1fd18fbfdde040c519fb39fb7
Eido NABESHIMA <closer009@gmail.com>
2014-03-12 20:21:46 +0900

    確認画面

--- app/controllers/consumers/registrations_controller.rb
+++ app/controllers/consumers/registrations_controller.rb
@@ -1,2 +1,10 @@
 class Consumers::RegistrationsController < Devise::RegistrationsController
+  def create
+    build_resource(sign_up_params)
+    if params[:confirm] && resource.valid?
+      render 'confirm'
+    else
+      super
+    end
+  end
 end
