d327b2d6b0088f2c9c1863c6339e493a35e84d4d
Eido NABESHIMA <closer009@gmail.com>
2014-03-13 19:45:21 +0900

    session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Commers::Application.config.session_store :redis_store
+if Rails.env.production?
+  Commers::Application.config.session_store :redis_store, key: "_session_id"
+else
+  Commers::Application.config.session_store :redis_store, key: "_development_session_id"
+end
