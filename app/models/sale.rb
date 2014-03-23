3ccf9f539225ab6c4387299ea3dfb4056191f2d3
Eido NABESHIMA <closer009@gmail.com>
2014-03-24 08:14:47 +0900

    年月日で絞り込み

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,7 +3,14 @@ class Sale
   attr_reader :name
 
   def self.search params
-    search_by_day
+    case params[:type]
+    when 'by_year'
+      search_by_year
+    when 'by_month'
+      search_by_month
+    else
+      search_by_day
+    end
   end
 
   def self.search_by_day
@@ -13,9 +20,17 @@ class Sale
   end
 
   def self.search_by_month
+    12.times.map(&:months).map do |months|
+      months_ago = months.ago
+      self.new("#{months_ago.year}年 #{months_ago.month}月", OrderItem.totaling.of_days(months_ago.beginning_of_month, months_ago.end_of_month))
+    end
   end
 
   def self.search_by_year
+    1.times.map(&:year).map do |years|
+      years_ago = years.ago
+      self.new("#{years_ago.year}年", OrderItem.totaling.of_days(years_ago.beginning_of_year, years_ago.end_of_year))
+    end
   end
 
   def self.search_by_products params
