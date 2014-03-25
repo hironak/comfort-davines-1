c1ecc6272d918f1fb2eeb69ab077e55b0effb43a
Eido NABESHIMA <closer009@gmail.com>
2014-03-26 05:07:06 +0900

    検索

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,4 +1,7 @@
 class Consumer < ActiveRecord::Base
+  include Authority::Abilities
+  self.authorizer_name = 'AdministrationAuthorizer'
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
@@ -10,6 +13,10 @@ class Consumer < ActiveRecord::Base
   has_many :orders
 
   after_initialize :initialize_setup
+
+  def total
+    orders.map(&:total_price).inject(:+).to_i
+  end
 
   def initialize_setup
     self.information ||= Consumer::Information.new
