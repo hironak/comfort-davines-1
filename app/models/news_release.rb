1f52673c72541975ef4d2fff5c6dd0ca5a369995
Eido NABESHIMA <closer009@gmail.com>
2014-04-01 16:10:55 +0900

    トピックスとニュースの順番

--- app/models/news_release.rb
+++ app/models/news_release.rb
@@ -4,6 +4,8 @@ class NewsRelease < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "190x242>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
+  default_scope -> { order(created_at: :desc) }
+
   scope :available, -> { where("date < ?", Date.tomorrow).order(date: :desc) }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,6 @@ class Topic < ActiveRecord::Base
   include Authority::Abilities
   self.authorizer_name = 'AdministrationAuthorizer'
 
+  default_scope -> { order(created_at: :desc) }
   scope :available, -> { where("date < ?", Date.tomorrow).order(date: :desc) }
 end
