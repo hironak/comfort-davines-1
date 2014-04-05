fb2a527790b81047cf978757e6cce274f86800f6
Eido NABESHIMA <closer009@gmail.com>
2014-04-05 15:14:19 +0900

    record_not_found

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,6 +1,7 @@
 class CatalogController < ApplicationController
   def series
     @catalog = Series.where(identify: params[:id]).first
+    raise ActiveRecord::RecordNotFound unless @catalog
     title @catalog.name
     add_breadcrumb @catalog.name
     @products = @catalog.products.available
@@ -15,6 +16,7 @@ class CatalogController < ApplicationController
 
   def category
     @catalog = Category.where(identify: params[:id]).first
+    raise ActiveRecord::RecordNotFound unless @catalog
     title "#{@catalog.name}一覧"
     add_breadcrumb @catalog.name
     @products = @catalog.products.available
@@ -24,6 +26,7 @@ class CatalogController < ApplicationController
 
   def solution
     @catalog = Solution.find(params[:id])
+    raise ActiveRecord::RecordNotFound unless @catalog
     title "お悩み別−#{@catalog.name}"
     add_breadcrumb @catalog.name
     @products = @catalog.products.available
