818222015962b42d7ec79cc3fe7016c0630bf4c6
Eido NABESHIMA <closer009@gmail.com>
2014-05-28 19:38:08 +0900

    agency

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class Agency < ActiveRecord::Base
   include Authority::Abilities
   self.authorizer_name = 'AdministrationAuthorizer'
@@ -9,10 +11,26 @@ class Agency < ActiveRecord::Base
 
   has_and_belongs_to_many :salons
   has_many :orders, through: :salons
+  attr_accessor :salon_csv
+
+  validates :name, presence: true
 
   after_initialize :set_defaults
 
+  before_save :save_salons_from_csv
+
   def set_defaults
     build_administrator unless administrator
+  end
+
+  private
+
+  def save_salons_from_csv
+    if self.salon_csv
+      csv = CSV.new(self.salon_csv)
+      csv.each do |row|
+
+      end
+    end
   end
 end
