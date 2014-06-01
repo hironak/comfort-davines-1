90938044f4dabb461e4caac67c2382d8ffec98b3
Eido NABESHIMA <closer009@gmail.com>
2014-06-02 00:50:14 +0900

    green

--- test/controllers/administration/margins_controller_test.rb
+++ test/controllers/administration/margins_controller_test.rb
@@ -3,48 +3,27 @@ require "test_helper"
 class Administration::MarginsControllerTest < ActionController::TestCase
 
   before do
-    @administration_margin = administration_margins(:one)
+    @agency = create :agency
   end
 
   def test_index
     get :index
     assert_response :success
-    assert_not_nil assigns(:administration_margins)
-  end
-
-  def test_new
-    get :new
-    assert_response :success
-  end
-
-  def test_create
-    assert_difference('Administration::Margin.count') do
-      post :create, administration_margin: {  }
-    end
-
-    assert_redirected_to administration_margin_path(assigns(:administration_margin))
+    assert_not_nil assigns(:agencies)
   end
 
   def test_show
-    get :show, id: @administration_margin
+    get :show, id: @agency
     assert_response :success
   end
 
   def test_edit
-    get :edit, id: @administration_margin
+    get :edit, id: @agency
     assert_response :success
   end
 
   def test_update
-    put :update, id: @administration_margin, administration_margin: {  }
-    assert_redirected_to administration_margin_path(assigns(:administration_margin))
-  end
-
-  def test_destroy
-    assert_difference('Administration::Margin.count', -1) do
-      delete :destroy, id: @administration_margin
-    end
-
-    assert_redirected_to administration_margins_path
+    put :update, id: @agency, agency: { product_margins_attributes: []  }
+    assert_redirected_to admin_margin_path(assigns(:agency))
   end
 end
