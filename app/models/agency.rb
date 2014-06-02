b807a56782c938036467c53141f2b90ff3a0bc05
Eido NABESHIMA <closer009@gmail.com>
2014-06-02 19:11:12 +0900

    代理店CSV

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -37,9 +37,30 @@ class Agency < ActiveRecord::Base
 
   def save_salons_from_csv
     if self.salon_csv
-      csv = CSV.new(self.salon_csv)
-      csv.each do |row|
+      CSV.new(self.salon_csv, headers: :first_row, col_sep: "\t").each do |attrs|
 
+        attrs = attrs.to_hash
+
+        puts attrs['name']
+        puts attrs['name_kana']
+
+        grades = %w|member platina premium|
+        attrs["grade"] = grades.detect{|l| attrs["grade_#{l}"] == "○" }
+        grades.map{|l| "grade_#{l}" }.each{|grade| attrs.delete(grade) }
+
+        if attrs['name']
+          current_salon = Salon.find_or_initialize_by(name: attrs['name'])
+          current_salon.attributes = attrs
+        else
+          attrs.each do |k, v|
+            if v
+              value = current_salon.send k
+              value = "#{value}\r\n#{v}"
+              current_salon.send "#{k}=", value
+            end
+          end
+        end
+        current_salon.save
       end
     end
   end
